a1d41d0b7963623d6281795e674f5f1565e458ac
Mats Honner <mobile.mats@gmail.com>
2021-12-07 07:39:24 +0100

    First part of day 5...

---
+++ day5.R
@@ -0,0 +1,73 @@
+library(tidyverse)
+
+wr_line <- function(b, l) { # board, line
+  if (l$is_horisontal_line) {
+    for (i in l$y1:l$y2) {
+      b[i, l$x1] <- b[i, l$x1] + 1
+    }
+  }
+  else {
+    for (i in l$x1:l$x2) {
+      b[l$y1, i] <- b[l$y1, i] + 1
+    }
+  }
+  return(b)
+}
+
+# test
+df <- readLines("c:\\R\\advent_code\\2021\\day5_test.txt")
+df <- data.frame(df)
+
+s <- str_extract_all(df$df, "[0-9]+", simplify = TRUE)
+colnames(s) <- c("x1", "y1", "x2", "y2")
+s <- data.frame(s)
+s[, 1] <- as.numeric(s[, 1])
+s[, 2] <- as.numeric(s[, 2])
+s[, 3] <- as.numeric(s[, 3])
+s[, 4] <- as.numeric(s[, 4])
+s <- s + 1 # adapt to R having matrices starting with '1'...
+
+# keep only horisontal and vertical lines
+s <- s %>% filter(x1 == x2 | y1 == y2)
+s$is_horisontal_line <- ifelse(s$x1 == s$x2, TRUE, FALSE)
+
+board <- matrix(0, nrow = 10, ncol = 10)
+
+s
+for (i in 1:nrow(s)) {
+  board <- wr_line(board, s[i, ])
+}
+board
+
+board[board == 1] <- 0
+board
+
+board[board > 1] <- 1
+sum(colSums(board))
+
+# 1
+df <- readLines("c:\\R\\advent_code\\2021\\day5.txt")
+df <- data.frame(df)
+
+s <- str_extract_all(df$df, "[0-9]+", simplify = TRUE)
+colnames(s) <- c("x1", "y1", "x2", "y2")
+s <- data.frame(s)
+s[, 1] <- as.numeric(s[, 1])
+s[, 2] <- as.numeric(s[, 2])
+s[, 3] <- as.numeric(s[, 3])
+s[, 4] <- as.numeric(s[, 4])
+s <- s + 1 # adapt to R having matrices starting with '1'...
+
+# keep only horisontal and vertical lines
+s <- s %>% filter(x1 == x2 | y1 == y2)
+s$is_horisontal_line <- ifelse(s$x1 == s$x2, TRUE, FALSE)
+
+board <- matrix(0, nrow = 1000, ncol = 1000)
+
+for (i in 1:nrow(s)) {
+  board <- wr_line(board, s[i, ])
+}
+
+board[board == 1] <- 0 # remove all single points
+board[board > 1] <- 1 # make all multiple points 1, to sum them up...
+sum(colSums(board))
